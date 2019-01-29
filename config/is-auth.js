74a41a11dc80889688fc2cdc298480c4788bb249
Rodolpho Netto <rodolpho.netto@gmail.com>
2019-01-28 23:25:33 -0200

    alterações de erros

--- config/is-auth.js
+++ config/is-auth.js
@@ -1,24 +1,30 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const rawToken = req.get('Authorization');
+  const rawToken = req.get("Authorization");
   if (!rawToken) {
-    res.json({ msg: 'Você não tem autorização para acessar essa area' });
+    res
+      .status(401)
+      .json({ msg: "Você não tem autorização para acessar essa area" });
   }
-  const token = rawToken.split(' ')[1];
+  const token = rawToken.split(" ")[1];
 
   let decodedToken;
   try {
     decodedToken = jwt.verify(
       token,
-      '!@#AquiNaoJaoAquiEProtegidoPorqueEuCrieiUmSegredoGrandePorqueONegocioDiziaQueEraFraco!@#',
+      "!@#AquiNaoJaoAquiEProtegidoPorqueEuCrieiUmSegredoGrandePorqueONegocioDiziaQueEraFraco!@#"
     );
   } catch (err) {
-    return res.json({ msg: 'Você não tem autorização para acessar essa area' });
+    return res
+      .status(404)
+      .json({ msg: "Você não tem autorização para acessar essa area" });
   }
 
   if (!decodedToken) {
-    return res.status(401).json({ msg: 'Você não tem autorização para acessar essa area' });
+    return res
+      .status(401)
+      .json({ msg: "Você não tem autorização para acessar essa area" });
   }
   req.userId = decodedToken.userid;
   req.username = decodedToken.username;
