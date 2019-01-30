eb7073e0058135dfde67370a86dd052f563e0511
Rodolpho Netto <rodolpho.netto@gmail.com>
2019-01-30 18:34:58 -0200

    teste

--- validation/country.js
+++ validation/country.js
@@ -1,44 +1,44 @@
-const Validator = require('validator');
-const isEmpty = require('./is-empty');
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
 
-exports.validateCountry = function (data) {
+exports.validateCountry = function(data) {
   const errors = {};
-  data.name = !isEmpty(data.name) ? data.name : '';
-  data.flag = !isEmpty(data.flag) ? data.flag : '';
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.flag = !isEmpty(data.flag) ? data.flag : "";
 
   if (Validator.isEmpty(data.name)) {
-    errors.nameEmpty = 'Nome de pais precisa ser preenchido';
+    errors.name = "Nome de pais precisa ser preenchido";
   }
 
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = 'Nome do pais precisa ter entre 2 e 30 caracteres';
+    errors.name = "Nome do pais precisa ter entre 2 e 30 caracteres";
   }
 
   if (Validator.isEmpty(data.flag)) {
-    errors.file = 'Selecione uma imagem para bandeira do pais';
+    errors.file = "Selecione uma imagem para bandeira do pais";
   }
 
   return {
     errors,
-    isValid: isEmpty(errors),
+    isValid: isEmpty(errors)
   };
 };
 
-exports.validateEditCountry = function (data) {
+exports.validateEditCountry = function(data) {
   const errors = {};
 
-  data.name = !isEmpty(data.name) ? data.name : '';
+  data.name = !isEmpty(data.name) ? data.name : "";
 
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = 'Nome do pais precisa ter entre 2 e 30 caracteres';
+    errors.name = "Nome do pais precisa ter entre 2 e 30 caracteres";
   }
 
   if (Validator.isEmpty(data.name)) {
-    errors.nameEmpty = 'Nome de pais precisa ser preenchido';
+    errors.name = "Nome de pais precisa ser preenchido";
   }
 
   return {
     errors,
-    isValid: isEmpty(errors),
+    isValid: isEmpty(errors)
   };
 };
