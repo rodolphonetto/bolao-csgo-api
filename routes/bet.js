ccac2fa8ced4c24b2225516709623bd87d168094
Rodolpho Netto <rodolpho.netto@gmail.com>
2019-02-22 15:51:23 -0300

    Atualização de rotas

--- routes/bet.js
+++ routes/bet.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
 
 const ObjectId = mongoose.Types.ObjectId;
@@ -8,72 +7,106 @@ const Match = require('../models/match');
 const User = require('../models/user');
 const validators = require('../validation/bet');
 
-let score;
-
-const getUsers = async () => {
-  const users = await User.find({ points: { $gt: 0 } }, { _id: 0, username: 1, points: 1 }).sort([
-    ['points', 'descending'],
-  ]);
-  return users;
-};
-
-const getMatches = async () => {
-  const matches = await Match.find({ finished: true })
-    .populate('teamA')
-    .populate('teamB');
-  return matches;
-};
-
+// Funções Globais
 const populateMatch = ID => Match.findById(ID)
   .populate('teamA')
   .populate('teamB');
 
 const verifyMatch = ID => Match.findById(ID);
 
-const updateBet = (matchID, userID, resultA, resultB, res) => {
-  const data = { resultA, resultB };
-
-  // Validação
+const inputValidation = (data) => {
   const { errors, isValid } = validators.validateBet(data);
   if (!isValid) {
-    return res.status(400).json({ errors, matchID });
+    return errors;
   }
-  //
+};
+//
 
-  Match.findOneAndUpdate(
-    { _id: ObjectId(matchID), 'bets.user': ObjectId(userID) },
-    { $set: { 'bets.$.resultA': resultA, 'bets.$.resultB': resultB } },
-    {
-      new: true,
-    },
-  )
-    .then((match) => {
-      populateMatch(match._id).then(bet => res.json(bet));
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+// Endpoint: /add-bet
+// Adicionar ou editar uma aposta
+const updateBet = async (betFields, res) => {
+  try {
+    const {
+      match: matchID, user: userID, resultA, resultB,
+    } = betFields;
+    const data = { resultA, resultB };
+    const errors = inputValidation(data);
+    if (errors) {
+      return res.status(400).json({ errors, matchID });
+    }
+
+    const match = await Match.findOneAndUpdate(
+      { _id: ObjectId(matchID), 'bets.user': ObjectId(userID) },
+      { $set: { 'bets.$.resultA': resultA, 'bets.$.resultB': resultB } },
+      {
+        new: true,
+      },
+    );
+    const bet = await populateMatch(match._id);
+    res.json(bet);
+  } catch (err) {
+    console.log(err);
+    res.status(400);
+  }
 };
 
-const newBet = (matchID, betFields, res) => {
-  const data = { resultA: betFields.resultA, resultB: betFields.resultB };
-  // Validação
-  const { errors, isValid } = validators.validateBet(data);
-  if (!isValid) {
-    return res.status(400).json({ errors, matchID });
-  }
-  //
+const newBet = async (betFields, res) => {
+  try {
+    const {
+      match: matchID, user: userID, resultA, resultB,
+    } = betFields;
 
-  Match.findOneAndUpdate({ _id: matchID }, { $push: { bets: betFields } }, { new: true })
-    .then((match) => {
-      populateMatch(match._id).then(bet => res.json(bet));
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+    const data = { resultA, resultB };
+    const errors = inputValidation(data);
+    if (errors) {
+      return res.status(400).json({ errors, matchID });
+    }
+    const match = await Match.findOneAndUpdate(
+      { _id: matchID },
+      { $push: { bets: betFields } },
+      { new: true },
+    );
+    const bet = await populateMatch(match._id);
+    res.json(bet);
+  } catch (err) {
+    console.log(err);
+    return res.status(400);
+  }
 };
 
+router.post('/add-bet', async (req, res) => {
+  const betFields = {};
+  const {
+    matchID, userID, resultA, resultB,
+  } = req.body;
+
+  if (resultA) betFields.resultA = resultA;
+  if (resultB) betFields.resultB = resultB;
+  if (matchID) betFields.match = matchID;
+  if (userID) betFields.user = userID;
+
+  const match = await verifyMatch(matchID);
+  if (match.open) {
+    const bets = await Match.aggregate([
+      {
+        $unwind: '$bets',
+      },
+      { $match: { _id: ObjectId(matchID), 'bets.user': ObjectId(userID) } },
+    ]);
+    if (bets.length > 0) {
+      updateBet(betFields, res);
+    } else {
+      newBet(betFields, res);
+    }
+  } else {
+    res.status(400).json({ msg: 'Você não pode apostar em uma partida fechada' });
+  }
+});
+
+// Endpoint: /fin-match/:matchID
+// Finanlizar as partidas e calcular o novo ranking
 const definePoints = (bet, match) => {
+  let score;
   if (bet.resultA === match.resultA && bet.resultB === match.resultB) {
     score = 5;
     return score;
@@ -99,42 +132,11 @@ const updateUserPoints = (match, res) => {
   });
 };
 
-// Adicionar/Editar nova aposta
-router.post('/add-bet', (req, res) => {
-  const betFields = {};
-  const matchID = req.body.matchID;
-  const userID = req.body.userID;
-
-  if (req.body.resultA) betFields.resultA = req.body.resultA;
-  if (req.body.resultB) betFields.resultB = req.body.resultB;
-  if (req.body.matchID) betFields.match = req.body.matchID;
-  if (req.body.userID) betFields.user = req.body.userID;
-
-  verifyMatch(matchID).then((match) => {
-    if (match.open) {
-      Match.aggregate([
-        {
-          $unwind: '$bets',
-        },
-        { $match: { _id: ObjectId(matchID), 'bets.user': ObjectId(userID) } },
-      ]).then((bets) => {
-        if (bets.length > 0) {
-          updateBet(matchID, userID, betFields.resultA, betFields.resultB, res);
-        } else {
-          newBet(matchID, betFields, res);
-        }
-      });
-    } else {
-      res.status(400).json({ msg: 'Vocẽ não pode apostar em uma partida fechada' });
-    }
-  });
-});
-
 router.put('/fin-match/:matchID', (req, res) => {
   const matchID = req.params.matchID;
 
   verifyMatch(matchID).then((match) => {
-    if (match.resultA >= 0 && match.resultB >= 0) {
+    if (match.resultA > 0 || match.resultB > 0) {
       Match.findOneAndUpdate({ _id: matchID }, { finished: true }, { new: true })
         .then((match) => {
           updateUserPoints(match, res);
@@ -149,10 +151,29 @@ router.put('/fin-match/:matchID', (req, res) => {
   });
 });
 
+// Endpoint: /ranking
+// Gerar o ranking dos usuarios por pontos
+const getRanking = async () => {
+  const users = await User.find({ points: { $gt: 0 } }, { _id: 0, username: 1, points: 1 }).sort([
+    ['points', 'descending'],
+  ]);
+  return users;
+};
+
 router.get('/ranking', async (req, res) => {
-  const users = await getUsers();
+  const users = await getRanking();
   res.json(users);
 });
+
+// Endpoint: /finished
+// Exibir as partidas finalizadas para os usuarios
+
+const getMatches = async () => {
+  const matches = await Match.find({ finished: true })
+    .populate('teamA')
+    .populate('teamB');
+  return matches;
+};
 
 router.get('/finished', async (req, res) => {
   const matches = await getMatches();
